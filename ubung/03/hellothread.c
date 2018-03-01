f24a694050ecd328f0e10a5bb943a766180f8307
blupp <mathias.jenter@student.uni-tuebingen.de>
2018-03-01 10:11:37 +0100

    Aufgabe 3.1

---
+++ ubung/03/hellothread.c
@@ -0,0 +1,26 @@
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+static void *threadRunning (){
+  printf("new thread running \n");
+
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  pthread_t thread;
+
+  printf("main() \n");
+
+  pthread_create(&thread, NULL,&threadRunning, NULL );
+
+  pthread_join(thread, NULL);
+  printf("thread finished \n");
+
+  return EXIT_SUCCESS;
+
+}
