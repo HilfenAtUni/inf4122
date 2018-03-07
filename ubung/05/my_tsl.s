41db9c255c35d6c39dd12123609e0e47b73c7d8a
M.R.Z <zgd1348833@gmail.com>
2018-03-07 23:30:45 +0100

    Kommatare hinzufuegen

--- ubung/05/my_tsl.s
+++ ubung/05/my_tsl.s
@@ -4,7 +4,10 @@
 	.type	my_tsl, @function
 
 my_tsl:
-	xor		%rax,%rax
-	inc		%al
-	lock	xchg %al,(%rdi)
-	retq
+	xor		%rax,%rax		/* using 64-bit general-purpose register, and use
+								xor to set it to 0 */
+	inc		%al				/* increase value in 8-bit general-purpose register
+								al */
+	lock	xchg %al,(%rdi)	/* exchange the value of register rdi and al and
+								use prefix 'lock' to lock the register */
+	retq					/* gleich wie "popq %rip" */
